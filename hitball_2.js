c3a5aab5f18409d3171108772135090cb4fd40ee
fned-ship <72948810+fned-ship@users.noreply.github.com>
2022-06-07 13:04:03 +0200

    Add files via upload

---
+++ hitball_2.js
@@ -0,0 +1,79 @@
+/////// LOAD IMAGES ////////
+
+
+// const LEVEL_IMG = new Image();
+// LEVEL_IMG.src = "level.png";
+
+// const LIFE_IMG = new Image();
+// LIFE_IMG.src = "life.png";
+
+// const SCORE_IMG = new Image();
+// SCORE_IMG.src = "score.png";
+
+const LEVEL_IMG = new Image();
+LEVEL_IMG.src = "prize.png";
+
+const LIFE_IMG = new Image();
+LIFE_IMG.src = "heart.png";
+
+const SCORE_IMG = new Image();
+SCORE_IMG.src = "star.png";
+
+
+
+// LOAD BG IMAGE
+// const BG_IMG = new Image();
+// BG_IMG.src = "bg.jpg";
+
+// const BG_IMG = new Image();//1920 * 1200  
+// BG_IMG.src = "galaxy1.jpg";
+
+var BG_IMG = new Image();//1000 * 1500 /* level1 */
+BG_IMG.src = "galaxy2.jpg";
+
+// const BG_IMG = new Image(512, 1024);//256 * 512
+// BG_IMG.src = "galaxy3.jpg";
+// const BG_IMG = new Image();//1920 * 1200
+// BG_IMG.src = "galaxy4.jpg";
+
+// const BG_IMG = new Image();//1000 * 1500 /* level2 */
+// BG_IMG.src = "nebula.jpg";
+
+// const BG_IMG = new Image();//1920 * 1200
+// BG_IMG.src = "galaxy01.jpg";
+
+// const BG_IMG = new Image();//1000 * 1500 /* level3 */
+// BG_IMG.src = "galaxy02.jpg";
+
+// const BG_IMG = new Image(512, 1024);//256 * 512
+// BG_IMG.src = "galaxy03.png";
+
+
+
+/////// END LOAD IMAGES ////////
+
+// ************************ //
+
+/////// LOAD SOUNDS ////////
+
+const WALL_HIT = new Audio();
+WALL_HIT.src = "wall.mp3";
+
+const LIFE_LOST = new Audio();
+LIFE_LOST.src = "life_lost.mp3";
+
+const PADDLE_HIT = new Audio();
+PADDLE_HIT.src = "paddle_hit.mp3";
+
+const WIN = new Audio();
+WIN.src = "win.mp3";
+
+const BRICK_HIT = new Audio();
+BRICK_HIT.src = "brick_hit.mp3";
+
+/////// END LOAD SOUNDS ////////
+const BARA = new Audio();
+BARA.src = "bara.mp3";
+
+const YANO = new Audio();
+YANO.src = "yano.mp3";
